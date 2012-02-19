becb11ed75d9bf985886d1549bc91b6cc6340be9
unknown <zhaochang@zc.(none)>
2012-02-19 15:00:38 +0800

    first commit

---
+++ application/modules/folder/test/controllers/home_made.php
@@ -0,0 +1,28 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Folder_Test_Home_Made_module extends CI_Module {
+
+	/**
+	 * 构造函数
+	 *
+	 * @return void
+	 * @author
+	 **/
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	function index()
+	{
+		// 这是装载本模块的模型，如果在本模块下找不到，则自动装载全局模型
+		$this->load->model('Main_data_model');
+		$this->Main_data_model->start();
+
+		// 装载全局模型
+		$this->load->model('Test_model');
+		$this->Test_model->abc();
+
+		$this->load->view('view_test');
+	}
+}
---
+++ application/modules/folder/test/models/main_data_model.php
@@ -0,0 +1,16 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Folder_Test_Main_data_model extends CI_Model {
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	function start()
+	{
+		echo '<p>Folder_Test_Main_data_model</p>';
+	}
+
+}
+?>
